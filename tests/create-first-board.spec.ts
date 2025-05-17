571f71ca36377d1e15383be4467700b5c4475a12
Arthur B <a.beszkid@sportradar.com>
2025-05-17 15:50:46 +0200

    Refactor create first board test to use base fixture and improve page object usage

--- tests/create-first-board.spec.ts
+++ tests/create-first-board.spec.ts
@@ -1,12 +1,10 @@
-import {test, expect, Locator} from '@playwright/test';
-import {GetStartedPage} from "../src/pages/get-started.page";
+import {test} from "../src/fixtures/base.fixture";
 
-test('create first board', async ({ page }) => {
+test('create first board', async ({ getStartedPage }) => {
     // Test Data
     const boardName = "Test Board";
 
-    const getStartedPage = new GetStartedPage(page)
-    await getStartedPage.goToPage()
+    await getStartedPage.goToPage("")
     await getStartedPage.firstBoardInput.fill(boardName);
 
     await getStartedPage.assertFirstBoardCreated(boardName);
