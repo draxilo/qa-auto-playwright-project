dd24a809d4c956196ce1909ba56e0eeb8071c240
Arthur B <a.beszkid@sportradar.com>
2025-05-17 15:52:02 +0200

    Refactor GetStartedPage to extend BasePage

--- src/pages/get-started.page.ts
+++ src/pages/get-started.page.ts
@@ -1,16 +1,12 @@
 import {expect, Locator, Page} from "@playwright/test";
+import {BasePage} from "./base.page";
 
-export class GetStartedPage {
-    private readonly page: Page;
+export class GetStartedPage extends BasePage {
     readonly firstBoardInput: Locator;
 
     constructor(page: Page) {
-        this.page = page;
+        super(page)
         this.firstBoardInput = page.getByTestId("first-board");
-    }
-
-    async goToPage() {
-        await this.page.goto("")
     }
 
     // Assertions
