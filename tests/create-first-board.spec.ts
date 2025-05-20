f8d0143010349f279f4ff715a955dd5353dc9375
Arthur B <a.beszkid@sportradar.com>
2025-05-20 11:01:35 +0200

    Add setup to delete existing boards before creating a new one

--- tests/create-first-board.spec.ts
+++ tests/create-first-board.spec.ts
@@ -1,4 +1,11 @@
 import {test} from "../src/fixtures/base.fixture";
+import {expect} from "@playwright/test";
+
+test.beforeEach(async ({request}) => {
+    const response = await request.delete("http://localhost:3000/api/boards")
+
+    expect(response.status()).toBe(204);
+})
 
 test('create first board', async ({ getStartedPage }) => {
     // Test Data
