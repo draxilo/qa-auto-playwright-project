1332499849a25c007d957fa85d202cb1d2cbd9f9
Arthur B <a.beszkid@sportradar.com>
2025-05-21 14:10:28 +0200

    Refactor delete-board.spec.ts to use pages.fixture.ts and improve board deletion test setup

--- tests/delete-board.spec.ts
+++ tests/delete-board.spec.ts
@@ -1,14 +1,32 @@
-import {test} from "../src/fixtures/base.fixture";
+import {test} from "../src/fixtures/pages.fixture";
+import {expect} from "@playwright/test";
 
-test("delete board", async ({ homePage, listPage }) => {
+const boardName = "Test Board";
+let boardId: number;
+
+// Set up
+test.beforeEach(async ({request}) => {
+    const response = await request.post("http://localhost:3000/api/boards", {
+        data: {
+            "name": boardName,
+        },
+    })
+
+    expect(response.status()).toBe(201);
+    const responseBody = await response.json();
+    boardId = responseBody.id; // Store the board ID
+})
+
+
+test("delete board", async ({ page, homePage, listPage }) => {
     // Test Data
 
-    await homePage.goToPage("");
+    await homePage.goToPage(`http://localhost:3000/board/${boardId}`)
 
-    await homePage.lastBoardItem.click();
     await listPage.boardOptionsButton.click();
     await listPage.deleteBoardButton.click();
 
+    await page.reload()
     // Assertions
-    await homePage.boardIsDeleted(await homePage.lastBoardItemName);
+    await homePage.boardIsDeleted(boardName);
 })
