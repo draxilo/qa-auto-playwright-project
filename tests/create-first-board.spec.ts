f6b192ca68cc2f108e54c3f63f329cf17ebfc8e2
Arthur B <a.beszkid@sportradar.com>
2025-05-21 14:11:15 +0200

    Refactor create-first-board.spec.ts to use pages.fixture.ts, add teardown logic, and improve board creation test setup

--- tests/create-first-board.spec.ts
+++ tests/create-first-board.spec.ts
@@ -1,18 +1,27 @@
-import {test} from "../src/fixtures/base.fixture";
+import {test} from "../src/fixtures/pages.fixture";
 import {expect} from "@playwright/test";
 
+// Set up
 test.beforeEach(async ({request}) => {
     const response = await request.delete("http://localhost:3000/api/boards")
 
     expect(response.status()).toBe(204);
 })
 
-test('create first board', async ({ getStartedPage }) => {
+test('create first board', async ({ page, getStartedPage, listPage }) => {
     // Test Data
     const boardName = "Test Board";
 
     await getStartedPage.goToPage("")
     await getStartedPage.firstBoardInput.fill(boardName);
+    await page.keyboard.press("Enter");
 
-    await getStartedPage.assertFirstBoardCreated(boardName);
+    await listPage.assertFirstBoardCreated(boardName);
 });
+
+// Tear down
+test.afterEach(async ({request}) => {
+    const response = await request.delete("http://localhost:3000/api/boards")
+
+    expect(response.status()).toBe(204);
+})
