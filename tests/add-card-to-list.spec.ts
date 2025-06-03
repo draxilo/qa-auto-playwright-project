f55aa0d9634ac4de263dd96d5f9ccac03931baf0
Arthur B <a.beszkid@sportradar.com>
2025-06-03 13:17:47 +0200

    Refactor add-card-to-list test to use Board interface for improved structure and clarity

--- tests/add-card-to-list.spec.ts
+++ tests/add-card-to-list.spec.ts
@@ -1,27 +1,30 @@
 import {test} from "../src/fixtures/pages.fixture";
 import {expect} from "@playwright/test";
 import {faker} from "@faker-js/faker";
+import {Board} from "../src/interfaces/board.interface";
 
-const boardName = faker.lorem.words(3); // Generate a random board name
+const board: Board = {
+    name: faker.lorem.words(3)
+}
+
 const listName = faker.lorem.words(3); // Name of the list to which the card will be added
-let boardId: number; // Declare a variable to store the board ID
 
 // Set up
 test.beforeEach(async ({request}) => {
     const response = await request.post("http://localhost:3000/api/boards", {
         data: {
-            "name": boardName,
+            "name": board.name,
         },
     })
 
     expect(response.status()).toBe(201);
     const responseBody = await response.json();
-    boardId = responseBody.id; // Store the board ID
+    board.id = responseBody.id; // Store the board ID
 
     const responseList = await request.post(`http://localhost:3000/api/lists`, {
         data: {
             "name": listName,
-            "boardId": boardId,
+            "boardId": board.id,
         },
     })
 
@@ -32,19 +35,23 @@ test("add card to list", async ({homePage, listPage }) => {
     // Test Data
     const cardTitleInput = faker.lorem.words(3); // Generate a random card title
 
+    await test.step('Navigate to the board and add a card', async () => {
+        await homePage.goToPage(`http://localhost:3000/board/${board.id}`)
+    })
 
-    await homePage.goToPage(`http://localhost:3000/board/${boardId}`)
+    await test.step('Add a card to the list', async () => {
+        await listPage.addAnotherCardButton.click();
+        await listPage.cardTitleInput.fill(cardTitleInput);
+        await listPage.addCardButton.click()
+    })
 
-    await listPage.addAnotherCardButton.click();
-    await listPage.cardTitleInput.fill(cardTitleInput);
-    await listPage.addCardButton.click()
-
-    //Assertions
-    await listPage.assertCardCreated(cardTitleInput);
+    await test.step('Assert Card is added to the list', async () => {
+        await listPage.assertCardCreated(cardTitleInput);
+    })
 })
 
 // Tear down
 test.afterEach(async ({request}) => {
-    const response = await request.delete(`http://localhost:3000/api/boards/${boardId}`);
+    const response = await request.delete(`http://localhost:3000/api/boards/${board.id}`);
     expect(response.status()).toBe(200);
 });
