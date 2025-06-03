677ec3733b1ede9716ebbae91bfd86a58b539efe
Arthur B <a.beszkid@sportradar.com>
2025-06-03 13:18:14 +0200

    Refactor add-list-to-board test to utilize Board interface for improved clarity and structure

--- tests/add-list-to-board.spec.ts
+++ tests/add-list-to-board.spec.ts
@@ -1,28 +1,24 @@
 import {expect} from "@playwright/test";
-import {test} from "../src/fixtures/pages.fixture";
+import {test} from "../src/fixtures/base.fixture";
 import {faker} from "@faker-js/faker";
+import {Board} from "../src/interfaces/board.interface";
 
-const boardName =  faker.lorem.words(3);
-let boardId: number; // Declare a variable to store the board ID
+const board: Board = {
+    name: faker.lorem.words(3)
+}
 
 // Set up
-test.beforeEach(async ({request}) => {
-    const response = await request.post("http://localhost:3000/api/boards", {
-        data: {
-            "name": boardName,
-        },
-    })
-
-    expect(response.status()).toBe(201);
+test.beforeEach(async ({ apiCreateBoard }) => {
+    const response = await apiCreateBoard(board);
     const responseBody = await response.json();
-    boardId = responseBody.id; // Store the board ID
+    board.id = responseBody.id; // Store the board ID
 })
 
 test('add list to board', async ({ homePage, listPage }) => {
     // Test Data
     const listName =  faker.lorem.words(3);
 
-    await homePage.goToPage(`http://localhost:3000/board/${boardId}`)
+    await homePage.goToPage(`http://localhost:3000/board/${board.id}`)
 
     //Dirty fix for the test to work
     await listPage.clickInTheMiddle();
@@ -37,6 +33,6 @@ test('add list to board', async ({ homePage, listPage }) => {
 
 // Tear down
 test.afterEach(async ({request}) => {
-    const response = await request.delete(`http://localhost:3000/api/boards/${boardId}`);
+    const response = await request.delete(`http://localhost:3000/api/boards/${board.id}`);
     expect(response.status()).toBe(200);
 });
