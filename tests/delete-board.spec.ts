8a2ac8c9ee45ca92ab1894268c24345dd2c4d381
Arthur B <a.beszkid@sportradar.com>
2025-06-04 15:25:53 +0200

    Refactor delete-board.spec.ts to use API helpers for board creation, improving test setup and clarity

--- tests/delete-board.spec.ts
+++ tests/delete-board.spec.ts
@@ -1,33 +1,27 @@
-import {test} from "../src/fixtures/pages.fixture";
-import {expect} from "@playwright/test";
+import {test} from "../src/fixtures/base.fixture";
 import { faker } from '@faker-js/faker';
+import {Board} from "../src/interfaces/board.interface";
 
-const boardName =  faker.lorem.words(3);
-let boardId: number; // Declare a variable to store the board ID
-
+const board: Board = {
+    name: faker.lorem.words(3),
+}
 // Set up
-test.beforeEach(async ({request}) => {
-    const response = await request.post("http://localhost:3000/api/boards", {
-        data: {
-            "name": boardName,
-        },
-    })
-
-    expect(response.status()).toBe(201);
-    const responseBody = await response.json();
-    boardId = responseBody.id; // Store the board ID
+test.beforeEach(async ({apiCreateList, apiCreateBoard}) => {
+    const createBoardResponse = await apiCreateBoard(board)
+    const createBoardResponseBody = await createBoardResponse.json();
+    board.id = createBoardResponseBody.id; // Store the board ID
 })
 
 
 test("delete board", async ({ page, homePage, listPage }) => {
     // Test Data
 
-    await homePage.goToPage(`http://localhost:3000/board/${boardId}`)
+    await homePage.goToPage(`http://localhost:3000/board/${board.id}`)
 
     await listPage.boardOptionsButton.click();
     await listPage.deleteBoardButton.click();
 
     await page.reload()
     // Assertions
-    await homePage.boardIsDeleted(boardName);
+    await homePage.boardIsDeleted(board.name);
 })
