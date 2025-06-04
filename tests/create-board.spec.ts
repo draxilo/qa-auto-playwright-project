fc418a6fe1266062ed1d6226dba5dfe1ea29a06b
Arthur B <a.beszkid@sportradar.com>
2025-06-04 14:46:16 +0200

    Refactor create-board.spec.ts to use API helpers for board creation and deletion, enhancing test clarity and maintainability

--- tests/create-board.spec.ts
+++ tests/create-board.spec.ts
@@ -1,23 +1,23 @@
-import {test} from "../src/fixtures/pages.fixture";
 import {expect} from "@playwright/test";
 import {faker} from "@faker-js/faker";
+import {test} from "../src/fixtures/base.fixture";
+import {Board} from "../src/interfaces/board.interface";
 
-let firstBoardId: number; // Declare a variable to store the board ID
+
+let url: string
+let createdBoardId: string
+const board: Board = {
+    name: faker.lorem.words(3)
+}
 
 // Set up
-test.beforeEach(async ({request}) => {
-    const response = await request.post("http://localhost:3000/api/boards", {
-        data: {
-            "name":  faker.lorem.words(3),
-        },
-    })
-
-    expect(response.status()).toBe(201);
-    const responseBody = await response.json();
-    firstBoardId = responseBody.id; // Store the board ID
+test.beforeEach(async ({apiCreateList, apiCreateBoard}) => {
+    const createBoardResponse = await apiCreateBoard(board)
+    const createBoardResponseBody = await createBoardResponse.json();
+    board.id = createBoardResponseBody.id; // Store the board ID
 })
 
-test("create board", async ({ homePage, listPage }) => {
+test("create board", async ({ homePage, listPage, page }) => {
     // Test Data
     const boardName =  faker.lorem.words(3);
 
@@ -27,13 +27,18 @@ test("create board", async ({ homePage, listPage }) => {
     await homePage.newBoardInput.fill(boardName);
     await homePage.createBoardButton.click();
 
+    createdBoardId = await homePage.waitForUrlAndGetLastPathSegment()
+
     // Assertions
     await listPage.assertBoardCreated(boardName);
 })
 
 // Tear down
-test.afterEach(async ({request}) => {
-    const response = await request.delete("http://localhost:3000/api/boards")
-
-    expect(response.status()).toBe(204);
-})
+test.afterEach(async ({apiDeleteListOfBoards}) => {
+    const boardToDelete = [board.id, createdBoardId].map(Number); // Removes falsy values
+    await apiDeleteListOfBoards(boardToDelete);
+});
+// test.afterEach(async ({request}) => {
+//     expect((await request.delete(`http://localhost:3000/api/boards/${board.id}`)).status()).toBe(200);
+//     expect((await request.delete(`http://localhost:3000/api/boards/${createdBoardId}`)).status()).toBe(200);
+// })
