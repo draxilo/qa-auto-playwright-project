de9ef9dd6e0262d353e5add5cb7359ead7d0b340
Arthur B <a.beszkid@sportradar.com>
2025-06-04 14:10:47 +0200

    Add API methods for deleting a board and creating multiple lists for enhanced test data management

--- src/fixtures/api-test-data.fixture.ts
+++ src/fixtures/api-test-data.fixture.ts
@@ -1,10 +1,13 @@
 import {APIResponse, expect} from "@playwright/test";
 import {Board} from "../interfaces/board.interface";
 import { test as base } from "@playwright/test";
+import {List} from "../interfaces/list.interface";
 
 
 interface ApiTestDataFixture {
     apiCreateBoard(board: Board): Promise<APIResponse>;
+    apiDeleteBoard(board: Board): Promise<APIResponse>;
+    apiCreateList(board: Board): Promise<Array<APIResponse>>;
 }
 
 export const test = base.extend<ApiTestDataFixture>({
@@ -18,6 +21,30 @@ export const test = base.extend<ApiTestDataFixture>({
 
             expect(response.status()).toBe(201);
             return response;
+        })
+    },
+    apiDeleteBoard: async ({ baseURL, request }, use) => {
+        await use(async (board: Board) => {
+            const response = await request.delete(`${baseURL}/api/boards/${board.id}`);
+            expect(response.status()).toBe(200);
+            return response;
+        })
+    },
+    apiCreateList: async ({ baseURL, request }, use) => {
+        await use(async (board: Board) => {
+            const responses: Array<APIResponse> = []
+            for (const list of board.lists) {
+                const response = await request.post(`${baseURL}/api/lists`, {
+                    data: {
+                        "name": list.name,
+                        "boardId": board.id,
+                    },
+                });
+
+                expect(response.status()).toBe(201);
+                responses.push(response);
+            }
+            return responses;
         })
     }
 })
