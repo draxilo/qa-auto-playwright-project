7ebd6580dae2df5ff7a3b7ea619cf58a7c5e28b7
Arthur B <a.beszkid@sportradar.com>
2025-09-10 14:23:50 +0200

    Adding .env file to project

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -16,7 +16,9 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* default timeout */
-  timeout: 5000,
+  expect: {
+    timeout: 3000,
+  },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -39,13 +41,14 @@ export default defineConfig({
   use: {
     testIdAttribute: 'data-cy',
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
 
+    actionTimeout: 3000,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     /* Screenshot on failure */
     screenshot: 'only-on-failure',
-
+    /* Video recording */
     video: 'retain-on-failure',
   },
 
