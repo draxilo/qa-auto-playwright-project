66118033658dff87bc5b7023e25cf0784ee1d7a7
Arthur B <a.beszkid@sportradar.com>
2025-09-26 10:00:38 +0200

    Update Playwright configuration for smoke and all tests; remove unused dependencies

--- playwright.web.config.ts
+++ playwright.web.config.ts
@@ -91,9 +91,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm start --prefix ../trelloapp-vue-vite-ts',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
