ff2337b35c3b8f81ca5c5c2e194b4dcb8a3b0be2
Arthur B <a.beszkid@sportradar.com>
2025-11-17 13:11:22 +0100

    Add global setup scripts for API and web tests to clear data before execution

---
+++ tests/api/global.api.setup.ts
@@ -0,0 +1,18 @@
+import { test as setup } from '@fixtures/base.fixture';
+import { step } from 'allure-js-commons';
+import { addAllure, AllureParams } from '@utils/allure.util';
+import { PARENT_SUITE, SUITE } from '@consts/suites.const';
+
+const allure: AllureParams = {
+  description: 'Global setup to clear out all data before tests.',
+  parentSuite: PARENT_SUITE.API,
+  suite: SUITE.SETUP,
+};
+
+setup('Setup', async ({ apiDeleteAllBoards }) => {
+  await addAllure(allure);
+
+  await step('Clear out all data', async () => {
+    await apiDeleteAllBoards();
+  });
+});
